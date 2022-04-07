2b9f765847230701ae7a9d70a60e2f878fc1ceb0
PAJEAN <pierre-antoine.jean@mines-ales.fr>
2022-04-07 11:15:32 +0200

    Add function to boilerplate.

--- js/components/boilerplate.js
+++ js/components/boilerplate.js
@@ -1,28 +1,29 @@
 try {
     (function() {
         const COMPONENT_NAME = 'wc-boilerplate';
-        
+
         const TEMPLATE = document.createElement('template');
         TEMPLATE.innerHTML = /* html */`
 
             <style></style>
-            
+
             <div id="main"></div>
 
         `;
-        
+
         window.customElements.define(COMPONENT_NAME, class extends HTMLElement {
             constructor() {
                 super();
                 this.attachShadow({mode: 'open'}); /* ShadowRoot */
             }
-          
+
             connectedCallback() {
                 this.shadowRoot.appendChild(TEMPLATE.content.cloneNode(true));
                 this.content = this.shadowRoot.querySelector('#main');
             }
-          
+
             disconnectedCallback() {}
+						attributeChangedCallback(attrName, oldVal, newVal) {}
         });
     })();
 }
